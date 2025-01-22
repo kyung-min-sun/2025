3ebea479ec5eccca9aeb8cce2eb360b7511bcceb
kevinsun127 <kevinsun@usc.edu>
2025-01-21 18:58:44 -0800

    resolutions

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { Tooltip, TooltipTrigger } from "~/components/ui/tooltip";
 import { TooltipContent, TooltipProvider } from "~/components/ui/tooltip";
@@ -5,12 +7,12 @@ import { TooltipContent, TooltipProvider } from "~/components/ui/tooltip";
 import { Button } from "~/components/ui/button";
 import { Check } from "lucide-react";
 import Link from "next/link";
+import { api } from "~/trpc/react";
 import { cn } from "~/lib/utils";
-import { db } from "~/server/db";
 import { myResolutions } from "../my-resolutions";
 
-export default async function CalendarPage() {
-  const resolutions = await db.resolution.findMany();
+export default function CalendarPage() {
+  const { data: resolutions } = api.resolution.getResolutions.useQuery();
 
   const days = new Array(365)
     .fill(0)
@@ -34,7 +36,6 @@ export default async function CalendarPage() {
     completed: completed(day),
     percent: completed(day) / myResolutions.length,
   }));
-  console.log(percents);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4">
@@ -44,7 +45,7 @@ export default async function CalendarPage() {
         </CardHeader>
         <CardContent>
           <div className="flex flex-row flex-wrap gap-[3px]">
-            {percents.map(({ day, percent, completed }) => (
+            {percents.map(({ day, percent }) => (
               <TooltipProvider
                 key={day.toLocaleDateString()}
                 delayDuration={10}
@@ -53,11 +54,11 @@ export default async function CalendarPage() {
                   <TooltipTrigger>
                     <div
                       className={cn(
-                        "bg-muted h-6 w-6 overflow-hidden rounded-md",
+                        "h-6 w-6 overflow-hidden rounded-md bg-muted",
                       )}
                     >
                       <div
-                        className="bg-primary h-full w-full"
+                        className="h-full w-full bg-primary"
                         style={{ opacity: percent }}
                       ></div>
                     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import { Button } from "~/components/ui/button";
 import { Calendar } from "lucide-react";
 import Link from "next/link";
 import { ResolutionCard } from "./resolution-card";
-import { db } from "~/server/db";
+import { api } from "~/trpc/react";
 
-export default async function Home() {
-  const resolutions = await db.resolution.findMany();
+export default function Home() {
+  const { data: resolutions } = api.resolution.getResolutions.useQuery();
 
   const todayResolutions = resolutions?.filter((resolution) => {
     return resolution.createdAt.toDateString() === new Date().toDateString();
@@ -13,7 +15,9 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <ResolutionCard todaysResolutions={todayResolutions} />
+      {todayResolutions && (
+        <ResolutionCard todaysResolutions={todayResolutions} />
+      )}
       <Button asChild variant="outline">
         <Link href="/calendar">
           <Calendar className="size-4" />
